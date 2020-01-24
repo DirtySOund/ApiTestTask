52d29072698403093513320141043d28d85849fa
Alexey Ilchuk <icq234908085@gmail.com>
2020-01-24 18:49:37 +0200

    TZ_update

--- Test_dummy/libs/api_requests.py
+++ Test_dummy/libs/api_requests.py
@@ -1,75 +1,38 @@
-import logging
 from json import JSONDecodeError
-
-import allure
 import requests
-
-logger = logging.getLogger(__name__)
 
 
 class RequestTypes:
     GET = 'GET'
     POST = 'POST'
-    DELETE = 'DELETE'
 
 
-def _send_request(request_type, url, request_data=None, custom_header=None, auth=False, auth_user=None):
-    _log_request(request_type, url, request_data)
+def _send_request(request_type, url, request_data=None):
     headers = ''
-    if custom_header:
-        headers = custom_header
-    elif auth and not custom_header:
-        if auth_user is not None:
-            headers = {'Authorization': ''}
     assert request_type in RequestTypes.__dict__
-
     if request_type == RequestTypes.GET:
         response = requests.get(url, json=request_data, headers=headers)
     elif request_type == RequestTypes.POST:
         response = requests.post(url, json=request_data, headers=headers)
     else:
-        response = requests.delete(url, json=request_data, headers=headers)
+        response = None
 
     try:
         response_body = response.json()
     except JSONDecodeError:
         response_body = None
-
-    _log_response(response.status_code, response_body)
     return response.status_code, response_body
 
 
-@allure.step(title="request")
-def _log_request(request_type, url, request_data):
-    logger.info(f'Request: {request_type} {url} Body: {request_data}')
-
-
-@allure.step(title="response")
-def _log_response(response_code, response_data):
-    logger.info(f'Response: {response_code} {response_data}')
-
-
 class ApiRequests:
-    """ Api requests """
+    """ Requests """
 
     def __init__(self, host):
         self.dummy_url = host
 
-    """ MVP requests """
-
-    def single_employee_try_get(self, user_id, custom_cache=None):
-        return _send_request(RequestTypes.GET, url=f'{self.dummy_url}/employee/{user_id}', custom_header=custom_cache)
-
     def single_employee_get(self, user_id):
-        status_code, response_data = self.single_employee_try_get(user_id)
-        assert status_code == 200, f'Wrong response status code: {status_code}'
-        return response_data
-
-    def single_employee_try_post(self, config, custom_cache=None):
-        return _send_request(RequestTypes.POST, url=f'{self.dummy_url}/create',
-                             request_data=config, custom_header=custom_cache)
+        return _send_request(RequestTypes.GET, url=f'{self.dummy_url}/employee/{user_id}')
 
     def single_employee_post(self, config):
-        status_code, response_data = self.single_employee_try_post(config)
-        assert status_code == 200, f'Wrong response status code: {status_code}'
-        return response_data
+        return _send_request(RequestTypes.POST, url=f'{self.dummy_url}/create',
+                             request_data=config)
--- Test_dummy/test_api/test_dummy_mvp.py
+++ Test_dummy/test_api/test_dummy_mvp.py
@@ -1,52 +1,53 @@
-from libs import random
-from libs.utils import Step
+from faker import Faker
+
+faker = Faker()
 
 
-def test_api_simple_requests(api_requests):
-    with Step('1. Create new user'):
-        config = random.random_user()
-        response_data= api_requests.single_employee_post(config)
-        user_id = response_data['data']['id']
-        api_requests.single_employee_get(user_id)
+def random_user():
+    return {
+        "name": faker.first_name(),
+        "salary": faker.pyint(min_value=100, max_value=10000, step=100),
+        "age": faker.pyint(min_value=21, max_value=80, step=1),
+    }
 
 
-def test_api_mvp(api_requests):
-    with Step('1. Create new user'):
-        config = random.random_user()
-        user_name, user_salary, user_age = config['name'], config['salary'], config['age']
-        status_code, response_data = api_requests.single_employee_try_post(config)
-        assert response_data['status'] == "success"
-        assert status_code == 200
-        user_id = response_data['data']['id']
+def test_api_positive_value(api_requests):
+    """1. Create new user"""
+    config = random_user()
+    user_name, user_salary, user_age = config['name'], config['salary'], config['age']
+    status_code, response_data = api_requests.single_employee_post(config)
+    assert response_data['status'] == "success"
+    assert status_code == 200
+    user_id = response_data['data']['id']
 
-    with Step('2. Get created user'):
-        status_code = api_requests.single_employee_try_get(user_id)
-        assert response_data['status'] == "success"
-        assert len(response_data['data']) == 4
-        assert response_data['data']['name'] == user_name
-        assert response_data['data']['salary'] == user_salary
-        assert response_data['data']['age'] == user_age
-        assert response_data['data']['id'] == user_id
-        assert status_code[0] == 200
+    """2. Get created user"""
+    status_code = api_requests.single_employee_get(user_id)
+    assert response_data['status'] == "success"
+    assert len(response_data['data']) == 4
+    assert response_data['data']['name'] == user_name
+    assert response_data['data']['salary'] == user_salary
+    assert response_data['data']['age'] == user_age
+    assert response_data['data']['id'] == user_id
+    assert status_code[0] == 200
 
 
-def test_api_error_handling(api_requests):
-    with Step('1. Try post new user with empty body'):
-        config = ''
-        status_code = api_requests.single_employee_try_post(config)
-        assert status_code[0] == 520
+def test_api_negative_value(api_requests):
+    """1. Try post new user with empty body"""
+    config = ''
+    status_code = api_requests.single_employee_post(config)
+    assert status_code[0] == 520
 
-    with Step('2. Try post new user with not full body'):
-        config = {"salary": "123", "age": "23"}
-        status_code = api_requests.single_employee_try_post(config)
-        assert status_code[0] == 200
+    """2. Try post new user with not full body"""
+    config = {"salary": "123", "age": "23"}
+    status_code = api_requests.single_employee_post(config)
+    assert status_code[0] == 200
 
-    with Step('3. Try get user with incorrect user_id'):
-        user_id = '/'
-        status_code = api_requests.single_employee_try_get(user_id)
-        assert status_code[0] == 404
+    """3.Try get user with incorrect user_id"""
+    user_id = '/'
+    status_code = api_requests.single_employee_get(user_id)
+    assert status_code[0] == 404
 
-    with Step('4. Try get user with un-existing user_id'):
-        user_id = '1111111111'
-        status_code = api_requests.single_employee_try_get(user_id)
-        assert status_code[0] == 200
+    """4.Try get user with un-existing user_id"""
+    user_id = '1111111111'
+    status_code = api_requests.single_employee_get(user_id)
+    assert status_code[0] == 200
